a0ce55e484b1f2c5c935c6c9cc96d106cd680c7a
ABDO.M <54600999+6sangoku9@users.noreply.github.com>
2025-11-21 14:31:27 +0000

    Add files via upload

---
+++ process_creation.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+pid_t pid = fork();
+
+if (pid == 0){
+printf("this is the child process.PID:%d\n",getpid());
+} else if(pid > 0){
+printf("this is the parent process .PID:%d",getpid());
+}else{
+printf("fork failed!\n");
+}
+return 0;
+}
